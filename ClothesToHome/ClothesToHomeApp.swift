69c3ebec97b27467424430fe8265ffc5cb606fc6
gallivas <102565784+gallivas@users.noreply.github.com>
2025-04-10 15:26:46 -0400

    Initial Commit

---
+++ ClothesToHome/ClothesToHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothesToHomeApp.swift
+//  ClothesToHome
+//
+//  Created by Stephen Gallivan on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ClothesToHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
