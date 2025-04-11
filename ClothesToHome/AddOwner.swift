1517a59c8ed55a62fd83f28864325f304ccd44e3
gallivas <102565784+gallivas@users.noreply.github.com>
2025-04-11 13:26:17 -0400

    Added AddOwner view and functionality to save a new Owner model.

---
+++ ClothesToHome/AddOwner.swift
@@ -0,0 +1,18 @@
+//
+//  AddOwner.swift
+//  ClothesToHome
+//
+//  Created by Stephen Gallivan on 4/11/25.
+//
+
+import SwiftUI
+
+struct AddOwner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddOwner()
+}
