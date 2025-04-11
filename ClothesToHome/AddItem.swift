750db3d6d07d7a89e6affcefc2aa69759555fcc2
gallivas <102565784+gallivas@users.noreply.github.com>
2025-04-11 15:22:27 -0400

    Created ability to add new owner. Working on Functionality and views supporting adding Items and Locations.

---
+++ ClothesToHome/AddItem.swift
@@ -0,0 +1,18 @@
+//
+//  AddItem.swift
+//  ClothesToHome
+//
+//  Created by Stephen Gallivan on 4/11/25.
+//
+
+import SwiftUI
+
+struct AddItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddItem()
+}
---
+++ ClothesToHome/LocationDetails.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetails.swift
+//  ClothesToHome
+//
+//  Created by Stephen Gallivan on 4/11/25.
+//
+
+import SwiftUI
+
+struct LocationDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetails()
+}
