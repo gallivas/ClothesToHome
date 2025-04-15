e11aa37abdf234bbbc8f1a089f2443b59d758e0b
gallivas <102565784+gallivas@users.noreply.github.com>
2025-04-15 14:56:53 -0400

    Added ability to add a new location and view those location details from the Owner view

---
+++ ClothesToHome/AddLocation.swift
@@ -0,0 +1,18 @@
+//
+//  AddLocation.swift
+//  ClothesToHome
+//
+//  Created by Stephen Gallivan on 4/15/25.
+//
+
+import SwiftUI
+
+struct AddLocation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddLocation()
+}
